ac81f86dc91bbc8abcc22be8b25984f34e2ecd56
min9yu98 <kmkkms9698@naver.com>
2022-09-06 20:06:47 +0900

    delete messageInfo

--- SpringBootTemplate/src/main/java/com/example/demo/src/message/MessageService.java
+++ SpringBootTemplate/src/main/java/com/example/demo/src/message/MessageService.java
@@ -20,14 +20,14 @@ import static com.example.demo.src.message.MessageController.numStr;
 public class MessageService {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     public void certifiedPhoneNumber(String phoneNumber, String cerNum) {
-        String api_key = "NCSHZED2LAFPK3ZP";
-        String api_secret = "1NOA9TFKEXMW9UBZBSVQS7AB5VPMG3CK";
+        String api_key = "";
+        String api_secret = "";
         Message coolsms = new Message(api_key, api_secret);
 
         // 4 params(to, from, type, text) are mandatory. must be filled
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("to", phoneNumber);    // 수신전화번호
-        params.put("from", "01022703842");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("from", "");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
         params.put("type", "SMS");
         params.put("text", "핫띵크 휴대폰인증 테스트 메시지 : 인증번호는" + "["+cerNum+"]" + "입니다.");
         params.put("app_version", "test app 1.2"); // application name and version
